8267e4a44257168ee66fc65861528bfee7990bd2
gadmel <abramov.gleb@gmail.com>
2023-02-22 16:47:17 +0100

    Make bikeApiService, make bikeApiHook, write sass styles for gallery and app components

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.scss';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.scss'
 
-import BikeGallery from '../BikeGallery/BikeGallery';
+import BikeGallery from '../BikeGallery/BikeGallery'
+import useBikesApi from '../../hooks/useBikesApi'
 
 function App() {
-   const mockBikes = [{ id: 'some ID', title: 'Trend-a-bikes MARK II' }];
+   const { bikes, loading } = useBikesApi()
 
    return (
       <div id="app">
          <header className="header">Frontend will be here</header>
          <main className="main">
             <Routes>
-               <Route path="/" element={<BikeGallery bikes={mockBikes} />} />
+               <Route path="/" element={!loading && <BikeGallery bikes={bikes} />} />
             </Routes>
          </main>
          <footer className="footer">Footer - Impressum</footer>
       </div>
-   );
+   )
 }
 
-export default App;
+export default App
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.sass';
-import App from './components/App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <React.StrictMode>
+      <Router>
+         <App />
+      </Router>
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
