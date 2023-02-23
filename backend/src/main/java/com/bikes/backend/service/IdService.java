05362b09965e07380cb06bce666eb61cc0e4c844
helenrendich <helen.rendich@wannweil.de>
2023-02-23 12:19:04 +0100

    add IdService and addBike (Frontend)

---
+++ backend/src/main/java/com/bikes/backend/service/IdService.java
@@ -0,0 +1,12 @@
+package com.bikes.backend.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class IdService {
+    public String generateId(){
+        return UUID.randomUUID().toString();
+    }
+}
