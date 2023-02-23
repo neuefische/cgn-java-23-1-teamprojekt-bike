6b09c4ed0042315a20871bc1321856bf71912a47
gadmel <abramov.gleb@gmail.com>
2023-02-23 21:15:08 +0100

    Catch errors on api-requests, as Johannes suggested

--- frontend/src/services/bikesApiService.ts
+++ frontend/src/services/bikesApiService.ts
@@ -3,11 +3,19 @@ import axios from 'axios'
 const apiUrlSlug = '/api/bikes/'
 
 async function get() {
-   return await axios.get(apiUrlSlug).then((response) => response.data)
+   return await axios
+      .get(apiUrlSlug)
+      .then((response) => response.data)
+      .catch((error) => {
+         console.error(error)
+      })
 }
 
 async function post(newBikeTitle: string) {
-   return await axios.post(apiUrlSlug, { title: newBikeTitle }).then((response) => response.data)
+   return await axios
+      .post(apiUrlSlug, { title: newBikeTitle })
+      .then((response) => response.data)
+      .catch((error) => console.error(error))
 }
 
 export default {
