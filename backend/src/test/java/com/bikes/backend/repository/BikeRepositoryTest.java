3c167e10013739b2b9887c50071068cc3d22d6f3
gadmel <abramov.gleb@gmail.com>
2023-02-28 18:48:32 +0100

    Fix repository unit tests - getAllBikes

--- backend/src/test/java/com/bikes/backend/repository/BikeRepositoryTest.java
+++ backend/src/test/java/com/bikes/backend/repository/BikeRepositoryTest.java
@@ -1,38 +1,59 @@
 package com.bikes.backend.repository;
 
 import com.bikes.backend.model.Bike;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataMongoTest
 class BikeRepositoryTest {
 	Bike testBike;
-	BikeRepository nonEmptyRepo;
-	BikeRepository testRepoEmpty;
+	@Autowired
+	BikeRepository repository;
 
-//    @BeforeEach
-//    public void setUp() {
-//        testBike = new Bike("testId", "testBike");
-//        nonEmptyRepo = new BikeRepository(Map.of(testBike.id(), testBike));
-//        testRepoEmpty = new BikeRepository(new HashMap<>());
-//    }
-//
-//    @Test
-//    void getAllBikesEmptyRepo() {
-//        //GIVEN
-//        List<Bike> expectedBikes = new ArrayList<>();
-//        //WHEN
-//        List<Bike> actualBikes = testRepoEmpty.getAllBikes();
-//        //THEN
-//        assertEquals(expectedBikes, actualBikes);
-//    }
-//
-//    @Test
-//    void getAllBikesNonEmptyRepo() {
-//        //GIVEN
-//        List<Bike> expectedBikes = List.of(testBike);
-//        //WHEN
-//        List<Bike> actualBikes = nonEmptyRepo.getAllBikes();
-//        //THEN
-//        assertEquals(expectedBikes, actualBikes);
-//    }
-//
+	@BeforeEach
+	public void setUp() {
+		testBike = new Bike("testId", "testBike");
+	}
+
+	@DisplayName("getAllBikes()")
+	@Nested
+	class testGetAllBikes {
+
+		@Test
+		@DisplayName("...should return an empty array if there are no bikes in the database")
+		@DirtiesContext
+		void getAllBikes_returnsEmptyArrayIfRepoIsEmpty() {
+			//GIVEN
+			List<Bike> expectedBikes = new ArrayList<>();
+			//WHEN
+			List<Bike> actualBikes = repository.findAll();
+			//THEN
+			assertEquals(expectedBikes, actualBikes);
+		}
+
+		@Test
+		@DisplayName("...should return all bikes if there are bikes in the database")
+		@DirtiesContext
+		void getAllBikes_returnsAllBikesIfRepoIsNotEmpty() {
+			//GIVEN
+			List<Bike> expectedBikes = List.of(testBike);
+			repository.save(testBike);
+			//WHEN
+			List<Bike> actualBikes = repository.findAll();
+			//THEN
+			assertEquals(expectedBikes, actualBikes);
+		}
+
+	}
 
 }
