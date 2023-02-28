067f41df4971e5afa41cd20199d93d6c038ae915
gadmel <abramov.gleb@gmail.com>
2023-02-28 19:17:31 +0100

    Fix repository unit tests - getBikeById, save

--- backend/src/test/java/com/bikes/backend/repository/BikeRepositoryTest.java
+++ backend/src/test/java/com/bikes/backend/repository/BikeRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.bikes.backend.repository;
 
 import com.bikes.backend.model.Bike;
+import com.bikes.backend.service.NoSuchBikeException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,6 +12,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -55,5 +57,71 @@ class BikeRepositoryTest {
 		}
 
 	}
+
+	@DisplayName("getBikeById()")
+	@Nested
+	class testGetBikeById {
+
+		@Test
+		@DisplayName("...should return a bike if the bike with the given id does exist")
+		@DirtiesContext
+		void getBikeById_returnsABikeIfThereIsABikeWithTheGivenId() {
+			//GIVEN
+			repository.save(testBike);
+			//WHEN
+			Bike actualBike = repository.findById(testBike.id()).orElseThrow(NoSuchBikeException::new);
+			//THEN
+			assertEquals(testBike, actualBike);
+		}
+
+		@Test
+		@DisplayName("...should return an empty optional if the bike with the given id does not exist")
+		@DirtiesContext
+		void getBikeById_returnsEmptyOptionalIfThereIsNoBikeWithTheGivenId() {
+			//GIVEN
+			String someIdThatDoesNotExist = "666";
+			Optional<Bike> expectedBike = Optional.empty();
+			//WHEN
+			Optional<Bike> actualBike = repository.findById(someIdThatDoesNotExist);
+			//THEN
+			assertEquals(expectedBike, actualBike);
+		}
+
+	}
+
+	@DisplayName("save()")
+	@Nested
+	class testAddBike {
+
+		@Test
+		@DisplayName("...should add bike to the database if the bike does not exist yet")
+		@DirtiesContext
+		void save_addsBikeToDatabase() {
+			//GIVEN
+			Bike expectedBike = testBike;
+			repository.findById(testBike.id()).ifPresent(repository::delete);
+			//WHEN
+			repository.save(testBike);
+			Bike actualBike = repository.findById(testBike.id()).get();
+			//THEN
+			assertEquals(expectedBike, actualBike);
+		}
+
+		@Test
+		@DisplayName("...should update the bike in the database if the bike already exists")
+		@DirtiesContext
+		void save_updatesBikeInDatabase() {
+			//GIVEN
+			Bike expectedBike = new Bike(testBike.id(), "updatedTitle");
+			repository.save(testBike);
+			//WHEN
+			repository.save(expectedBike);
+			Bike actualBike = repository.findById(testBike.id()).get();
+			//THEN
+			assertEquals(expectedBike, actualBike);
+		}
+
+	}
+
 
 }
