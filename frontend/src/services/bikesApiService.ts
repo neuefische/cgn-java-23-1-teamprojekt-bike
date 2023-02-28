9c61aed51d7d87a50e960b09d7eb89d44be22ce3
gadmel <abramov.gleb@gmail.com>
2023-02-28 15:49:04 +0100

    commit again #2

--- frontend/src/services/bikesApiService.ts
+++ frontend/src/services/bikesApiService.ts
@@ -3,27 +3,29 @@ import { Bike } from '../models/Bike'
 
 const apiUrlSlug = '/api/bikes/'
 
-async function get() {
-   return await axios
-      .get(apiUrlSlug)
-      .then((response) => response.data)
-      .catch((error) => {
-         console.error(error)
-      })
-}
+export default function bikeApiService() {
+   async function get() {
+      return await axios
+         .get(apiUrlSlug)
+         .then((response) => response.data)
+         .catch((error) => {
+            console.error(error)
+         })
+   }
 
-async function post(newBikeTitle: string) {
-   return await axios
-      .post(apiUrlSlug, { title: newBikeTitle })
-      .then((response) => response.data)
-      .catch((error) => console.error(error))
-}
+   async function post(newBikeTitle: string) {
+      return await axios
+         .post(apiUrlSlug, { title: newBikeTitle })
+         .then((response) => response.data)
+         .catch((error) => console.error(error))
+   }
 
-async function put(bikeToUpdate: Bike) {
-   return await axios
-      .put(apiUrlSlug, bikeToUpdate)
-      .then((response) => response.data)
-      .catch((error) => console.error(error))
-}
+   async function put(bikeToUpdate: Bike) {
+      return await axios
+         .put(apiUrlSlug, bikeToUpdate)
+         .then((response) => response.data)
+         .catch((error) => console.error(error))
+   }
 
-export { get, post, put }
+   return { get, post, put }
+}
