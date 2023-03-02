b4f5fe928280468adee7e437f859e594d696cd6e
gadmel <abramov.gleb@gmail.com>
2023-03-02 15:55:05 +0100

    Make some styling, fix the broken input align

--- frontend/src/components/BikeCard/BikeCard.tsx
+++ frontend/src/components/BikeCard/BikeCard.tsx
@@ -13,52 +13,67 @@ function BikeCard(props: BikeCardProps) {
    const [editMode, setEditMode] = useState(false)
    const [updatedTitle, setUpdatedTitle] = useState(props.bike.title)
 
-   function handleEditButton(){
+   function handleEditButton() {
       setEditMode(!editMode)
    }
-   function handleUpdateTitle(event: ChangeEvent<HTMLInputElement>){
+
+   function handleUpdateTitle(event: ChangeEvent<HTMLInputElement>) {
       setUpdatedTitle(event.target.value)
    }
-   function handleSaveChanges(){
-      props.editBike({...props.bike, title: updatedTitle})
+
+   function handleSaveChanges() {
+      props.editBike({ ...props.bike, title: updatedTitle })
    }
-   function handleCancelEdit(){
+
+   function handleCancelEdit() {
       setEditMode(false)
       setUpdatedTitle(props.bike.title)
    }
+
    function handleDelete() {
       props.deleteBike(props.bike.id)
    }
 
    return (
-       <>
-         {!editMode?
+      <>
+         {!editMode ? (
             <div className="card" id={props.bike.id}>
                <h2 className="card__title">{props.bike.title}</h2>
-               <div className={"card__controls"}>
+               <div className="card__controls">
                   <Link className="card__button" to={'details/' + props.bike.id}>
                      Go to details
                   </Link>
-                  <div  className={"card__controls--horizontal"}>
-                     <button onClick={handleEditButton} className={"card__button inline-half"}>Edit</button>
-                     <button onClick={handleDelete} className={"card__button inline-half"}>Delete this bike</button>
+                  <div className="card__controls--horizontal">
+                     <button onClick={handleEditButton} className="card__button inline-half">
+                        Edit
+                     </button>
+                     <button onClick={handleDelete} className="card__button inline-half">
+                        Delete this bike
+                     </button>
                   </div>
                </div>
-
-            </div> :
-             <div className={"card"}>
-                <label className={"card__edit-label"}>
-                 Title: <input className={"card__edit-input"} value={updatedTitle} onChange={handleUpdateTitle}/>
-                </label>
-                <div className={"card__controls"}>
-                   <div  className={"card__controls--horizontal"}>
-                      <button className="card__button inline-half" onClick={handleSaveChanges}>Save Changes</button>
-                      <button className="card__button inline-half" onClick={handleCancelEdit}>Save Cancel</button>
-                   </div>
-                </div>
-             </div>
-         }
-       </>
+            </div>
+         ) : (
+            <div className="card">
+               <label className="card__edit-label">
+                  <span className="card__edit-label--title">Title:</span>
+                  <div className="card__edit-input--wrapper">
+                     <input className="card__edit-input" value={updatedTitle} onChange={handleUpdateTitle} />
+                  </div>
+               </label>
+               <div className="card__controls">
+                  <div className="card__controls--horizontal">
+                     <button className="card__button inline-half" onClick={handleSaveChanges}>
+                        Save Changes
+                     </button>
+                     <button className="card__button inline-half" onClick={handleCancelEdit}>
+                        Save Cancel
+                     </button>
+                  </div>
+               </div>
+            </div>
+         )}
+      </>
    )
 }
 
