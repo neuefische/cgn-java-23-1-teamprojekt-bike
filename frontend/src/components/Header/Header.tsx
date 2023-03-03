8b5eaa1c0d95fda6b355d1c3563eb48ab241d546
helenrendich <helen.rendich@wannweil.de>
2023-03-03 10:20:43 +0100

    add set up navigation

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import './Header.css'
 import logo from '../../assets/logo.png'
 import title from '../../assets/title.png'
 import { useMediaQuery } from 'react-responsive'
+import {Link} from "react-router-dom";
 
 function Header() {
    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 900px)' })
@@ -10,12 +11,14 @@ function Header() {
       <header className="header">
          <div className={'header__container' + (isTabletOrMobile ? '' : ' horizontal')}>
             <div className="header__container--third">
-               <img className="header__logo" src={logo} alt="" />
+               <img className={"header__logo"+ (isTabletOrMobile ? '' : ' squared')} src={logo} alt="" />
             </div>
             <div className="header__container--third">
                <img className="header__logo" src={title} alt="Mega bikes 9000" />
             </div>
-            <div className="header__container--third"></div>
+            <nav className="header__container--third navigation">
+               <Link to={"/"}>Home</Link>
+            </nav>
          </div>
       </header>
    )
