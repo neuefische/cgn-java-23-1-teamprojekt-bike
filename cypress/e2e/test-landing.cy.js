47bacc024e7d4541c20215ae5bf58ab35f6f99d7
gadmel <abramov.gleb@gmail.com>
2023-03-05 15:13:12 +0100

    Work on #5

--- cypress/e2e/test-landing.cy.js
+++
@@ -1,5 +0,0 @@
-describe("template spec", () => {
-  it("visits the Mega Bike Master 9000's Landing page", () => {
-    cy.visit("localhost:3000");
-  });
-});
---
+++ frontend/cypress/e2e/test-landing.cy.js
@@ -0,0 +1,5 @@
+describe('Mega Bike Master 9000 - Landing page', () => {
+   it("visits the Mega Bike Master 9000's Landing page", () => {
+      cy.visit('localhost:3000')
+   })
+})
