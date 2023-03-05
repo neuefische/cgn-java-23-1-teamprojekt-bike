83637d9fa468554398b56f574f1884156f4a8274
gadmel <abramov.gleb@gmail.com>
2023-03-05 09:58:29 +0100

    Work on

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+  projectId: "vvu8e5",
+  fileServerFolder: "frontend",
+};
