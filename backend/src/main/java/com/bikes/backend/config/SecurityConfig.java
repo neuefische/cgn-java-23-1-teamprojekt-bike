5da285561b74f766bd1f9b5911354f87f8ac34d2
helenrendich <helen.rendich@wannweil.de>
2023-03-06 14:41:21 +0100

    add Argon2PasswordEncoder

--- backend/src/main/java/com/bikes/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/bikes/backend/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -17,11 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -36,5 +32,10 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 }
