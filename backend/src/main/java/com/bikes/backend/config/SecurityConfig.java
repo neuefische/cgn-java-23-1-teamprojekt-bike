483e6a241afd9e2189dbe3a5e120b8f1f687196f
helenrendich <helen.rendich@wannweil.de>
2023-03-06 11:11:42 +0100

    add filterChain in SecurityConfig

--- backend/src/main/java/com/bikes/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/bikes/backend/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.bikes.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -27,5 +30,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/api/**").authenticated()
+                .requestMatchers(HttpMethod.PUT,"/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and().build();
     }
 }
