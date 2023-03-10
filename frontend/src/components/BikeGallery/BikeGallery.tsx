253a339fb4aef70d4c4e7a619b0392bb1be92c28
gadmel <abramov.gleb@gmail.com>
2023-03-10 18:23:13 +0100

    Fix the login forwarding in bike gallery

--- frontend/src/components/BikeGallery/BikeGallery.tsx
+++ frontend/src/components/BikeGallery/BikeGallery.tsx
@@ -6,6 +6,8 @@ import AddBike from '../AddBike/AddBike'
 import useBikesApi from '../../hooks/useBikesApi'
 import Layout from '../Layout/Layout'
 import { useMediaQuery } from 'react-responsive'
+import { useNavigate } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 
 type BikeGalleryProps = {
    addBikeRef: React.MutableRefObject<HTMLFormElement>
@@ -13,7 +15,10 @@ type BikeGalleryProps = {
 }
 
 function BikeGallery(props: BikeGalleryProps) {
+   const currentUser = useAuth(false)
    const { bikes, addBike, editBike, deleteBike, loading } = useBikesApi()
+   const navigate = useNavigate()
+   !currentUser && navigate('/login')
 
    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 900px)' })
    return (
